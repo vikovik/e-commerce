58524d1795fef71203eec3d06a0490c1b40cc28a
vikovik <103561366+vikovik@users.noreply.github.com>
2023-06-22 17:48:37 -0400

    Add files via upload

---
+++ build/precache-manifest.15c9e392706af8930728c3b0a55a766e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd781c3c43837d5aaf6f",
+    "url": "/static/js/main.cd781c3c.chunk.js"
+  },
+  {
+    "revision": "327450801acb8f6d859e",
+    "url": "/static/js/1.32745080.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8929bbb341f8a31bd280d8d5bffdf9e8",
+    "url": "/index.html"
+  }
+];
