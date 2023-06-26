c7c7e350057910f2c7913de60b1c70d67e31b8b1
vikovik <103561366+vikovik@users.noreply.github.com>
2023-06-26 15:53:55 -0400

    Add files via upload

--- src/redux/wishlistRedux.js
+++ src/redux/wishlistRedux.js
@@ -1,31 +1,27 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const cartSlice = createSlice({
-  name: "cart",
+const wishlistSlice = createSlice({
+  name: "wishlist",
   initialState: {
     products: [],
     qty: 0,
-    totalPrice: 0,
   },
   reducers: {
-    addProduct: (state, action) => {
+    addProductList: (state, action) => {
       state.qty += action.payload.qty;
       state.products.push(action.payload);
-      state.totalPrice += action.payload.price * action.payload.qty;
     },
-    clearCart: (state) => {
+    clearWishlist: (state) => {
       state.products = [];
       state.qty = 0;
-      state.totalPrice = 0;
     },
-    removeFromCart: (state, action) => {
+    removeFromList: (state, action) => {
       const productId = action.payload;
       const removedProduct = state.products.find(
         (product) => product._id === productId
       );
       if (removedProduct) {
         state.qty -= removedProduct.qty;
-        state.totalPrice -= removedProduct.price * removedProduct.qty;
         state.products = state.products.filter(
           (product) => product._id !== productId
         );
@@ -34,5 +30,6 @@ const cartSlice = createSlice({
   },
 });
 
-export const { addProduct, clearCart, removeFromCart } = cartSlice.actions;
-export default cartSlice.reducer;
+export const { addProductList, clearWishlist, removeFromList } =
+  wishlistSlice.actions;
+export default wishlistSlice.reducer;
